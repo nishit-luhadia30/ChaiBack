e42b3d957ab97982851d35f2ba5dcca5ffd4b3ea
nishit-luhadia30 <nishit95luhadia1@gmail.com>
2025-07-11 12:42:24 +0530

    changes

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/health', (req, res) => {
-    res.send('<h1>Health Check OK</h1>');
+app.get('/api/jokes', (req, res) => {
+    const jokes = [
+        { id: 1, title: 'Joke 1', content: 'This is the first joke.' },
+        { id: 2, title: 'Joke 2', content: 'This is the second joke.' },
+        { id: 3, title: 'Joke 3', content: 'This is the third joke.' },
+        { id: 4, title: 'Joke 4', content: 'This is the fourth joke.' },
+        { id: 5, title: 'Joke 5', content: 'This is the fifth joke.' }
+    ];
+    res.json(jokes);
 })
 
 app.get('/api', (req, res) => {
